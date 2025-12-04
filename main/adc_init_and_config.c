e65b47da41e4e3a725d7f7e740af86d9277865a7
dine-1 <ratanprasanna3@gmail.com>
2025-12-04 10:18:22 +0530

    global vars to local in headers

--- main/adc_init_and_config.c
+++ main/adc_init_and_config.c
@@ -1,5 +1,6 @@
 #include "adc/adc_init_and_config.h"
 void adc_init_and_config(adc_continuous_handle_t *adc_handle){
+  //adc handle
   adc_continuous_handle_cfg_t adcHdlCfg={
     .max_store_buf_size=ADC_MEM_POOL_SIZE,
     .conv_frame_size=SOC_ADC_DIGI_DATA_BYTES_PER_CONV*2
@@ -7,13 +8,17 @@ void adc_init_and_config(adc_continuous_handle_t *adc_handle){
   //get adc handle
   adc_continuous_new_handle(&adcHdlCfg,adc_handle);
 
-  //channels
-  adcChnlsCfg[0]=(adc_digi_pattern_config_t){
-    .atten=ADC_ATTEN_DB_12,
-    .channel=ADC_CHANNEL_3,
-    .unit=1,
-    .bit_width=12,
+  //adc channels
+  adc_digi_pattern_config_t adcChnlsCfg[1]={
+    {
+      .atten=ADC_ATTEN_DB_12,
+      .channel=ADC_CHANNEL_3,
+      .unit=1,
+      .bit_width=12,
+    }
   };
+  //adc configuration
+  adc_continuous_config_t adcCfg;
   adcCfg=(adc_continuous_config_t){
     .pattern_num=1,
     .adc_pattern=adcChnlsCfg,
--- main/main.c
+++ main/main.c
@@ -1,19 +1,26 @@
+//for uint_type
+#include <stdint.h>
+//for NULL
+#include <stddef.h>
 //adc functions
 #include "esp_adc/adc_continuous.h"
 
 //initialise and configure the adc
 #include "adc/adc_init_and_config.h"
 
-void adc_setup(){
+void app_main(){
+  //adc handle
+  adc_continuous_handle_t adcHdl;
   //configure adc
   adc_init_and_config(&adcHdl);
-  //start adc
-  adc_continuous_start(adcHdl);
-}
 
-void app_main(){
-  //setup adc
-  adc_setup();
+  //start sampling adc
+  adc_continuous_start(adcHdl);
+
+  //adc results size
+  uint8_t results[ADC_MEM_POOL_SIZE];
+  //adc out length
+  uint32_t out_length;
   //reading the results of the adc
   adc_continuous_read(adcHdl,results,ADC_MEM_POOL_SIZE,&out_length,adc_timeout_ms);
 }
